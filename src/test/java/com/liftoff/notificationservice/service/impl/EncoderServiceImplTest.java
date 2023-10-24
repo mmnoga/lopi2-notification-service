5f80a5363dc51ff484dd19bcddbc1dda41949bd1
Marcin Noga <marcin@careaboutit.pl>
2023-10-24 18:50:01 +0200

    feature/L01-177: tests adjusted

--- src/test/java/com/liftoff/notificationservice/service/impl/EncoderServiceImplTest.java
+++ src/test/java/com/liftoff/notificationservice/service/impl/EncoderServiceImplTest.java
@@ -1,12 +1,14 @@
 package com.liftoff.notificationservice.service.impl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class EncoderServiceImplTest {
 
     @InjectMocks
